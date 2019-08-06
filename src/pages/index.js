60021d462138756e07f8a174237e2f916417b696
Jordan Papaleo <papaleowebdev@gmail.com>
2019-08-06 16:48:51 -0700

    let there be code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,62 @@
-import React from "react"
-import { Link } from "gatsby"
-
+import React, { useState } from "react"
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const [hashInput, setHashInput] = useState('')
+
+  function handleChange (e) {
+    setHashInput(e.target.value)
+  }
+
+  function onCopy () {
+
+  }
+
+  let formattedOutput = ''
+
+  try {
+    const cleaned = hashInput
+      .replace(/=>/g, ': ')
+      .replace(/nil/g, null)
+
+    const parsed = JSON.parse(cleaned)
+    const stringified = JSON.stringify(parsed, undefined, 2)
+    formattedOutput = stringified
+  } catch (err) {
+    console.log(err)
+  }
+
+  console.log('formattedOutput', formattedOutput)
+  const style = {
+    minHeight: 200,
+    border: '1px solid gray',
+    padding: 20,
+  }
+  return (
+    <Layout>
+      <div style={{display: 'grid', gridTemplateColumns: '1fr', gridGap: 20, padding: 20}}>
+        <p>Takes output from our tests and turns it into readable json</p>
+        <textarea
+          placeholder="Paste Hash Diff here"
+          onChange={handleChange}
+          value={hashInput}
+          style={{...style, maxHeight: 200}}
+        />
+        <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr', alignItems: 'center', gridGap: 20}}>
+          <p>Turn test output into json</p>
+          <CopyToClipboard onCopy={onCopy} text={formattedOutput}>
+            <button>Copy output to clipboard</button>
+          </CopyToClipboard>
+        </div>
+        <pre style={{...style, minHeight: 400, maxHeight: 800, oveflow: 'scroll'}}>
+          <code>
+            {formattedOutput}
+          </code>
+        </pre>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
